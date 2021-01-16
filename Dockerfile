8f22cbca27dff1c17c264322cb9b6b9a5a5d34a4
Ricardo Rea <rickirea@gmail.com>
2021-01-16 14:32:04 -0600

    Putting the front-dev environment in docker

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:alpine
+WORKDIR /app
+ENV PATH /app/node_modules/.bin:$PATH
+COPY ./package*.json ./
+RUN npm install
+RUN npm install react-scripts
+
+COPY . ./
+
+CMD ["npm", "start"]
--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,21 @@
-import React, { useRef } from 'react';
-import Map from './Map';
-import Button from './Button.jsx';
-import './App.css';
+import React, { useRef } from "react";
+import Map from "./Map";
+import Button from "./Button.jsx";
+import "./App.css";
 
 function App() {
   const requestPointsRef = useRef(() => {});
-  
+  console.log("App is working again...");
+
   return (
     <div id="app">
       <Map
         zoom={5}
         lat={19.453603}
-        lng={-99.140410}
+        lng={-99.14041}
         requestPoint={requestPointsRef}
       />
-      <Button
-        onClick={requestPointsRef}
-      />
+      <Button onClick={requestPointsRef} />
     </div>
   );
 }
